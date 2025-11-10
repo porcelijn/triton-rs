36d03492f4988a991cccf7c2f5246334cd4c1873
Tijn Porcelijn <tijn@juvoly.nl>
2025-11-10 10:09:59 +0100

    Use `Response` rather than hard-coded example

--- example-backend/src/lib.rs
+++ example-backend/src/lib.rs
@@ -1,26 +1,11 @@
 //! https://github.com/triton-inference-server/backend/blob/main/README.md#triton-backend-api
 
 use futures::executor::block_on;
-use libc::c_void;
 use std::ffi::CString;
 use std::ptr;
 use std::slice;
 use triton_rs::Backend;
-
-pub(crate) type BoxError = Box<dyn std::error::Error>;
-
-pub(crate) fn check_err(err: *mut triton_sys::TRITONSERVER_Error) -> Result<(), BoxError> {
-    if !err.is_null() {
-        let code = unsafe { triton_sys::TRITONSERVER_ErrorCode(err) };
-        Err(format!(
-            "TRITONBACKEND_ModelInstanceModel returned error code {}",
-            code
-        )
-        .into())
-    } else {
-        Ok(())
-    }
-}
+use triton_rs::Response;
 
 #[derive(Debug, Default)]
 struct InstanceState(usize);
@@ -30,6 +15,8 @@ impl InstanceState {
          self.0 += 1; // do stuff
     }
 }
+
+const SEND_FLAGS_FINAL: u32 = triton_sys::tritonserver_responsecompleteflag_enum_TRITONSERVER_RESPONSE_COMPLETE_FINAL;
 
 struct ExampleBackend;
 
@@ -116,61 +103,15 @@ impl Backend<InstanceState> for ExampleBackend {
                 output.print_info();
             }
 
-            let mut response: *mut triton_sys::TRITONBACKEND_Response = ptr::null_mut();
-            check_err(unsafe {
-                triton_sys::TRITONBACKEND_ResponseNew(&mut response, request.as_ptr())
-            })?;
+            let mut response = Response::new(request)?;
 
             let out = format!("you said: {prompt}");
             let encoded = triton_rs::encode_string(&out);
+            response.add_output("output",
+                triton_sys::TRITONSERVER_datatype_enum_TRITONSERVER_TYPE_BYTES,
+                &[1], &encoded)?;
 
-            {
-                let mut output: *mut triton_sys::TRITONBACKEND_Output = ptr::null_mut();
-                let name = CString::new("output").expect("CString::new failed");
-                let datatype = triton_sys::TRITONSERVER_datatype_enum_TRITONSERVER_TYPE_BYTES;
-                let shape = &[1];
-                let dims_count = 1;
-
-                check_err(unsafe {
-                    triton_sys::TRITONBACKEND_ResponseOutput(
-                        response,
-                        &mut output,
-                        name.as_ptr(),
-                        datatype,
-                        shape.as_ptr(),
-                        dims_count,
-                    )
-                })?;
-
-                {
-                    let mut buffer: *mut c_void = ptr::null_mut();
-                    let buffer_byte_size = encoded.len() as u64;
-                    let mut memory_type: triton_sys::TRITONSERVER_MemoryType = 0;
-                    let mut memory_type_id = 0;
-                    check_err(unsafe {
-                        triton_sys::TRITONBACKEND_OutputBuffer(
-                            output,
-                            &mut buffer,
-                            buffer_byte_size,
-                            &mut memory_type,
-                            &mut memory_type_id,
-                        )
-                    })?;
-
-                    let mem: &mut [u8] = unsafe {
-                        slice::from_raw_parts_mut(buffer as *mut u8, buffer_byte_size as usize)
-                    };
-
-                    mem.copy_from_slice(&encoded);
-                }
-            }
-
-            let send_flags =
-            triton_sys::tritonserver_responsecompleteflag_enum_TRITONSERVER_RESPONSE_COMPLETE_FINAL;
-            let err = ptr::null_mut();
-            check_err(unsafe {
-                triton_sys::TRITONBACKEND_ResponseSend(&mut *response, send_flags, err)
-            })?;
+            response.send(SEND_FLAGS_FINAL)?;
         }
 
         Ok(())
--- triton-rs/src/response.rs
+++ triton-rs/src/response.rs
@@ -1,6 +1,8 @@
 use crate::{check_err, Request, Error};
+use libc::c_void;
 use std::ffi::CString;
 use std::ptr;
+use std::slice;
 
 pub struct Response {
     ptr: *mut triton_sys::TRITONBACKEND_Response,
@@ -37,6 +39,29 @@ impl Response {
         })?;
         Ok(())
     }
+
+    fn output(&mut self, name: &str, data_type: u32, shape: &[i64]) -> Result<Output, Error> {
+        let name = CString::new(name)?;
+        let mut output: *mut triton_sys::TRITONBACKEND_Output = ptr::null_mut();
+        check_err(unsafe {
+            triton_sys::TRITONBACKEND_ResponseOutput(
+                self.ptr,
+                &mut output,
+                name.as_ptr(),
+                data_type,
+                shape.as_ptr(),
+                shape.len().try_into().unwrap(),
+            )
+        })?;
+
+        Ok(Output::from_ptr(output))
+    }
+
+    pub fn add_output(&mut self, name: &str, data_type: u32, shape: &[i64], data: &[u8]) -> Result<(), Error> {
+        let mut output = self.output(name, data_type, shape)?;
+        output.set_data(&data)?;
+        Ok(())
+    }
 }
 
 impl Drop for Response {
@@ -45,5 +70,39 @@ impl Drop for Response {
             triton_sys::TRITONBACKEND_ResponseDelete(self.ptr)
         };
         assert!(error == ptr::null_mut());
+    }
+}
+
+struct Output {
+    ptr: *mut triton_sys::TRITONBACKEND_Output,
+}
+
+impl Output {
+    fn from_ptr(ptr: *mut triton_sys::TRITONBACKEND_Output) -> Self {
+        Self { ptr }
+    }
+
+    pub fn set_data(&mut self, data: &[u8]) -> Result<(), Error> {
+        let mut buffer: *mut c_void = ptr::null_mut();
+        let buffer_byte_size = data.len() as u64;
+        let mut memory_type: triton_sys::TRITONSERVER_MemoryType = 0;
+        let mut memory_type_id = 0;
+        check_err(unsafe {
+            triton_sys::TRITONBACKEND_OutputBuffer(
+                self.ptr,
+                &mut buffer,
+                buffer_byte_size,
+                &mut memory_type,
+                &mut memory_type_id,
+            )
+        })?;
+
+        let mem: &mut [u8] = unsafe {
+            slice::from_raw_parts_mut(buffer as *mut u8, buffer_byte_size as usize)
+        };
+
+        mem.copy_from_slice(&data);
+
+        Ok(())
     }
 }
